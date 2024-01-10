356d168eaa93641bf868e0779f756be8390c148f
SantiXN <santixm123@gmail.com>
2024-01-10 10:10:00 +0300

    Перенес показ слайдов в MenuBar, исправил useEffect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,12 +7,7 @@ import {useAppActions, useAppSelector} from "../../store/types.ts";
 
 function Header() {
     const title = useAppSelector(state => state.presentation.title)
-    const currentSlide = useAppSelector(state => state.presentation.slides[state.presentation.indexOfCurrentSlide])
-    const { createSetSelectedBlockAction } = useAppActions()
     const ref = useRef<HTMLInputElement>(null)
-    const previewWindow = document.getElementById("preview-window")
-    const workSpace = (previewWindow != null) ? previewWindow!.children[0] as HTMLElement : null
-
 
     const { createChangeTitleAction } = useAppActions()
 
@@ -23,37 +18,7 @@ function Header() {
         const elem = ref.current!
         elem.addEventListener('keydown', onKeyDown)
         return () => elem.removeEventListener('keydown', onKeyDown)
-    }, [])
-
-    const previewMode = () => {
-        const block = document.getElementById(currentSlide.selectedBlockId!)
-        console.log(block)
-        if (block) {
-            block!.style.outline = "none"
-        }
-        createSetSelectedBlockAction(currentSlide.id, null)
-        previewWindow!.requestFullscreen()
-    }
-
-    useEffect(() => {
-        const onFullScreen = () => {
-            if (document.fullscreenElement)
-            {
-                workSpace!.style.transform = `scale(${window.innerWidth / 1660})`
-                workSpace!.style.overflow = `hidden`
-                previewWindow!.style.pointerEvents = 'none'
-            } else {
-                workSpace!.style.transform = ''
-                previewWindow!.style.pointerEvents = 'auto'
-                workSpace!.style.overflow = ``
-            }
-        }
-        document.addEventListener('fullscreenchange', onFullScreen)
-        return (() => {
-            document.removeEventListener('fullscreenchange', onFullScreen)
-        })
-    })
-
+    }, [title])
 
     return (
         <div className={styles.header}>
@@ -70,7 +35,6 @@ function Header() {
                     />
                     <MenuBar />
                 </div>
-                <button className={styles.button} onClick={() => previewMode()}>Просмотр</button>
             </div>
             <ToolBar />
         </div>
--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -2,12 +2,16 @@ import styles from "./MenuBar.module.css"
 import {useAppActions, useAppSelector} from "../../store/types.ts";
 import {Presentation} from "../../types/types.ts";
 import html2PDF from 'jspdf-html2canvas';
+import {useEffect} from "react";
 
 function MenuBar()
 {
     const title = useAppSelector(state => state.presentation.title)
+    const currentSlide = useAppSelector(state => state.presentation.slides[state.presentation.indexOfCurrentSlide])
     const slides = useAppSelector(state => state.presentation.slides)
-    const { createAddPresentationAction } = useAppActions()
+    const previewWindow = document.getElementById("preview-window")
+    const workSpace = (previewWindow != null) ? previewWindow!.children[0] as HTMLElement : null
+    const { createAddPresentationAction, createSetSelectedBlockAction } = useAppActions()
 
     function getURL(): string {
         const presentation: Presentation = {
@@ -65,12 +69,41 @@ function MenuBar()
           })
     }
 
+    const previewMode = () => {
+        const block = document.getElementById(currentSlide.selectedBlockId!)
+        if (block) {
+            block!.style.outline = "none"
+        }
+        createSetSelectedBlockAction(currentSlide.id, null)
+        previewWindow!.requestFullscreen()
+    }
+
+    useEffect(() => {
+        const onFullScreen = () => {
+            if (document.fullscreenElement)
+            {
+                workSpace!.style.transform = `scale(${window.innerWidth / 1660})`
+                workSpace!.style.overflow = `hidden`
+                previewWindow!.style.pointerEvents = 'none'
+            } else {
+                workSpace!.style.transform = ''
+                previewWindow!.style.pointerEvents = 'auto'
+                workSpace!.style.overflow = ``
+            }
+        }
+        document.addEventListener('fullscreenchange', onFullScreen)
+        return (() => {
+            document.removeEventListener('fullscreenchange', onFullScreen)
+        })
+    }, [])
+
     return (
         <div className="menu">
             <button className={styles.button}><label className={styles.text} htmlFor="file_uploads">Загрузить</label></button>
             <button className={styles.button}><a className={styles.text} href={getURL()} download={title}>Сохранить JSON</a></button>
             <button className={styles.button} onClick={() => getPdf()}><a className={styles.text}>Сохранить PDF</a></button>
             <input className={styles.input} id="file_uploads" type="file" accept="application/json" onChange={(event) => loadFile(event)}></input>
+            <button className={styles.button} onClick={() => previewMode()}>Просмотр</button>
         </div>
     )
 }
